652796b6037b0437b8daa1e2576cbd147ca35bcb
VishalDakalia3110 <vishalhain3110@gmail.com>
2024-09-03 18:46:48 +0530

    Remove and Update function Created

--- hotel.c
+++ hotel.c
@@ -464,10 +464,10 @@ void Rooms(MYSQL *conn) {
                         AddRoom(conn);
                         break;
                 case 3:
-                       // UpdateRoom(conn);
+                        UpdateRoom(conn);
                         break;
                 case 4:
-                       // RemoveRoom(conn);
+                        RemoveRoom(conn);
                         break;
                 case 5:
                        // SearchRoom(conn);
@@ -623,5 +623,115 @@ void AddRoom(MYSQL *conn) {
 
     printf("\nRoom Add Successfully...\n");
     
+}
+void UpdateRoom(MYSQL *conn) {
+    char query[256] = "SELECT * from Room_Details";
+    MYSQL_RES *result;
+    if(mysql_query(conn, query)) finish_with_error(conn);
+    result = mysql_store_result(conn);
+    if(result == NULL) finish_with_error(conn);
+    int row = mysql_num_rows(result);
+    mysql_free_result(result);
+    int R_no,Charges,R_type;
+    char rtype[50];
+    if(row == 0) {
+	printf("No Room Available to update...\n");
+	return;
+    }
+    else {
+    	printf("Updating Room...\n");
+	printf("Enter Room Number You want to update :- ");
+	scanf("%d",&R_no);
+	while(true) {
+		snprintf(query, sizeof(query), "SELECT * from Room_Details where R_no=%d", R_no);
+		if(mysql_query(conn, query)) finish_with_error(conn);
+		result = mysql_store_result(conn);
+		if(result == NULL) finish_with_error(conn);
+		row = mysql_num_rows(result);
+		mysql_free_result(result);
+		if(row == 0) {
+		    printf("Invalid Room Number Enter Again :- ");
+		    scanf("%d",&R_no);
+		}
+		else break;
+    	}
+	
+    }
+    printf("\nRoom types are:\n");
+    printf("     1. Single Room (Charges(per day) = Rs. 1500)\n");
+    printf("     2. Double Room (Charges(per day) = Rs. 2500)\n");
+    printf("     3. Family Room (Charges(per day) = Rs. 3000)\n");
+    printf("     4. Duplex Room (Charges(per day) = Rs. 5500)\n");
+    printf("     5. Conference Room (Charges(per day) = Rs. 8500)\n");
+    printf("     6. Inter-Connecting Room (Charges(per day) = Rs. 6000)\n");
+    while(true) {
+        printf("Choose any Room Type :- ");
+	scanf("%d",&R_type);
+	if (R_type <= 0 || R_type >= 7) {
+            printf("Invalid input, Please select valid input :- ");
+        } else {
+            if (R_type == 1) {
+                strcpy(rtype, "Single Room");
+                Charges = 1500;
+            } else if (R_type == 2) {
+                strcpy(rtype, "Double Room");
+                Charges = 2500;
+            } else if (R_type == 3) {
+                strcpy(rtype, "Family Room");
+                Charges = 3000;
+            } else if (R_type == 4) {
+                strcpy(rtype, "Duplex Room");
+                Charges = 5500;
+            } else if (R_type == 5) {
+                strcpy(rtype, "Conference Room");
+                Charges = 8500;
+            } else if (R_type == 6) {
+                strcpy(rtype, "Inter-Connecting Room");
+                Charges = 6000;
+            }
+	    printf("Siuuu");
+            break;
+        }
+
+    }
+    snprintf(query, sizeof(query), "UPDATE Room_Details set R_type='%s', Charges=%d where R_no=%d", rtype, Charges, R_no);
+    if(mysql_query(conn, query)) finish_with_error(conn);
+    printf("Room Updated Successfully...\n");
+
+}
+
+void RemoveRoom(MYSQL *conn) {
+    char query[256] = "SELECT * from Room_Details";
+    if(mysql_query(conn, query)) finish_with_error(conn);
+    MYSQL_RES *result;
+    int row;
+    result = mysql_store_result(conn);
+    if(result == NULL) finish_with_error(conn);
+    row = mysql_num_rows(result);
+    mysql_free_result(result);
+    if(row == 0) {
+        printf("No Room is Available to Remove\n");
+       return;	
+    }
+    int R_no;
+    while(true) {
+        printf("Enter Room Number you want to remove :- ");
+	scanf("%d",&R_no);
+	snprintf(query, sizeof(query), "SELECT * from Room_Details where R_no=%d", R_no);
+	if(mysql_query(conn, query)) finish_with_error(conn);
+	result = mysql_store_result(conn);
+	if(result == NULL) finish_with_error(conn);
+	row = mysql_num_rows(result);
+	if(row == 0) {
+	    printf("Enter Valid room number\n");
+	}
+	else {
+	    snprintf(query, sizeof(query), "DELETE from Room_Details where R_no=%d", R_no);
+	    if(mysql_query(conn, query)) finish_with_error(conn);
+	    printf("Room Removed Successfully...\n");
+	    break;
+	}
+
+    }
 }
 
